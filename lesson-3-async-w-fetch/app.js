fbf6e43c2392ecc3393b22461e36ef383e16dfd2
dujie <dujie0930@outlook.com>
2018-08-01 17:27:10 +0900

    111

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -1,12 +1,65 @@
-(function () {
+(function() {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
 
-    form.addEventListener('submit', function (e) {
+    form.addEventListener('submit', function(e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+            headers: {
+                Authorization: 'Client-ID bd18852d92f956937def8d7f0ab0f6d3fc1acf00cf096af4f143e2f1fd66fef0'
+            }
+        }).then(response => response.json()).then(addImage).catch(e => requestImageError(e, 'image'));
+
+        function addImage(data) {
+            let htmlContent = '';
+            const firstImage = data.results[0];
+
+            if (firstImage) {
+                htmlContent = `<figure>
+            <img src="${firstImage.urls.small}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`;
+            } else {
+                htmlContent = 'Unfortunately, no image was returned for your search.'
+            }
+
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        };
+
+        function requestImageError(e, part) {
+            console.log(e);
+            responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+        };
+
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=ed11ce69c5b2490e9a4c4e41e1aa686d`, {
+            // headers: {
+            //     Authorization: 'Client-ID bd18852d92f956937def8d7f0ab0f6d3fc1acf00cf096af4f143e2f1fd66fef0'
+            // }
+        }).then(response => response.json()).then(addArticles).catch(e => requestArticleError);
+
+        function addArticles(data) {
+            let htmlContent = '';
+            // const data = JSON.parse(this.responseText);
+            if (data.response && data.response.docs && data.response.docs.length > 1) {
+                htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article"> 
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                    </li>`).join('') + '</ul>';
+
+            } else {
+                htmlContent = ` < div class = "error-no-articles" > No articles available < /div>`;
+            }
+            responseContainer.insertAdjacentHTML('afterend', htmlContent);
+        };
+
+        function requestArticleError(e, part) {
+            console.log(e);
+            responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">There was an error making a request for the ${part}.</p>`);
+        };
     });
 })();
